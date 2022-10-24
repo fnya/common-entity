9e8569444b133b66641b1a4a988672a0e84cf076
fnya <fnyamail@gmail.com>
2022-10-24 23:49:30 +0900

    プロジェクト参照設定

---
+++ src/entity/OuterEntity.ts
@@ -0,0 +1,15 @@
+/**
+ * プロジェクト外部のエンティティ
+ */
+export class OuterEntity {
+  public age: number;
+
+  /**
+   * コンストラクタ
+   *
+   * @param age: 年齢
+   */
+  constructor(age: number) {
+    this.age = age;
+  }
+}
